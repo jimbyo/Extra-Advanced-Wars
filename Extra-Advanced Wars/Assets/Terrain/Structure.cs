e867190df7047756e403e1177f666b2a2850a83c
NoNeed2Shout <hjefamily@yahoo.com>
2018-01-12 18:02:05 -0800

    Did some Terrain stuff

---
+++ Extra-Advanced Wars/Assets/Terrain/Structure.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour {
+    public int income,team,capturePoints=20;
+    public bool isCritical;
+    //use isCritical for command centers. 0 for neutral team
+
+    public void capturing(int health) //to be called by capturing infantry
+    { capturePoints -= health; }
+    public int getIncome()
+    { return income; }
+    public int getTeam()
+    { return team; }
+    public bool criticalCheck() //to be checked on board after a building is captured - it is a win condition, afer all
+    { return isCritical; }
+
+    public void capture(int capturer) 
+    //to be called when structure is captured by infantry - most of capture process to be done in infantry script
+    { 
+        team = capturer;
+        resetCapture();
+    }
+
+    public void resetCapture() 
+    //called when a structure is captured or the capturing unit dies or moves - called from board whenever a unit's move is confirmed and
+    //infantry is no longer on a building
+    { capturePoints = 20; }
+}
--- Extra-Advanced Wars/Assets/Terrain/TerrainScript.cs
+++ Extra-Advanced Wars/Assets/Terrain/TerrainScript.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainScript : MonoBehaviour {
+    public float defenseMultiplier;
+    public int moveCost;
+    public bool passable;
+    //Assign those three variables in the prefabs
+    //All terrain-related prefabs need this. Road/Plain/etc will use only this script for terrain-related things, structures will also need
+    //a building script, production will also need the appropriate production script
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Terrain is passable if passable is true AND no enemy unit is on the tile. Terrain cannot be landed on if any unit is on it.
+    //Checking if a unit is on a tile will have to be done on the board (2d array) itself
+    public float getDefense() { return defenseMultiplier; }
+    public int getMoveCost() { return moveCost; }
+    public bool getPassable() { return passable; }
 }
