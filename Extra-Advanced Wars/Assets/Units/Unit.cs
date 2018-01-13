f650e38b5b6bf30a483103d91d4d9f9edc3baf58
NoNeed2Shout <hjefamily@yahoo.com>
2018-01-12 18:21:25 -0800

    Set up Unit class

--- Extra-Advanced Wars/Assets/Units/Unit.cs
+++ Extra-Advanced Wars/Assets/Units/Unit.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int moveSpeed, health;
+    public float damage, defense;
+
 }
