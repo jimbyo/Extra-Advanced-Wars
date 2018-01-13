c5956b5aa61ed7e94e98527c9ce477b3d468f84a
NoNeed2Shout <hjefamily@yahoo.com>
2018-01-12 16:47:20 -0800

    Deleted TerrainScript and created a new one inside the Terrain Folder

--- Extra-Advanced Wars/Assets/Terrain/TerrainScript.cs
+++ Extra-Advanced Wars/Assets/Terrain/TerrainScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainSpace : MonoBehaviour {
+public class TerrainScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
