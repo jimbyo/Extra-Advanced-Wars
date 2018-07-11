27b97284660029b805c5f51aee1e0ab76fb4aa70
Jimbyo <jimz@uci.edu>
2018-07-11 15:15:20 -0700

    dfsdfs

---
+++ Winter Quarter Unity WorkShop/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed,jump_force;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed* Time.deltaTime, rb.velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.AddForce(new Vector2(0, jump_force));
+        }
+	}
+    void FixedUpdate()
+    {
+
+    }
+}
